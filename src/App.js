db0f38c7ea014b3debba59800674841c8d902a4b
Shweta Ashok Ghale <123123785+ShwetaGhale@users.noreply.github.com>
2023-01-20 03:26:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,53 @@
+
+// import React from "react";
+// //import Todo from "./components/todoreact/todo";
+// import Temp from "./component/weather/weathercard";
+
+// const App = () => {
+//   return (
+//     <>
+//       <Temp />
+//     </>
+//   );
+// };
+
+// export default App;
+
+
+import React from "react";
+
+import Temp from "./Component/temp";
+
+const App = () => {
+  return (
+    <>
+      <Temp />
+    </>
+  );
+};
+
+export default App;
+
+
+
+
+// import React from "react";
+// //import ReactDOM from 'react-dom/client';
+
+// //import App from './App';
+// //import reportWebVitals from './reportWebVitals';
+
+// // const root = ReactDOM.createRoot(document.getElementById('root'));
+// // root.render(
+// //   <React.StrictMode>
+// //     <App />
+// //   </React.StrictMode>
+// // );
+
+
+// // reportWebVitals();
+
+// const App = () =>{
+//   return <div>hello Everyone</div>;
+// };
+// export default App;
